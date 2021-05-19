1689ae275dcffbf672322be08f9c95b8b68e6f11
Vitalii Vyhonskyi <vitaliy.v@ualeaders.com>
2021-05-19 12:49:07 +0300

    Google analytics added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,45 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { QuestionModel } from './models';
 import { QuestionsService } from './questions.service';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   questions$: Observable<QuestionModel[]>;
+  private routerEventsSubscription: Subscription;
 
   constructor(
     private questionsService: QuestionsService,
-    private route: ActivatedRoute
+    private router: Router,
   ) {
     this.questions$ = this.questionsService.getQuestions();
   }
 
   ngOnInit(): void {
+    this.routerEventsSubscription = this.router.events
+      .pipe(
+        tap(event => {
+          if (event instanceof NavigationEnd) {
+            gtag('config', 'G-1T7C355CND', { 'page_path': event.urlAfterRedirects });
+          }
+        })
+      )
+      .subscribe();
+  }
 
+  ngOnDestroy(): void {
+    if (this.routerEventsSubscription) {
+      this.routerEventsSubscription.unsubscribe();
+    }
   }
 }
